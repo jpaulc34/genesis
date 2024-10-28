f42cb9aae522c61d9b7d46a94e8cba93b85acd72
jpaulc34 <jpcabilan@gmail.com>
2024-10-29 00:54:33 +0800

    refactored to application factory pattern

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from app.routers import user_router, category_router, product_router
-
 from app.core.database import lifespan
 
-app = FastAPI(lifespan=lifespan)
+def create_app() -> FastAPI:
+    app = FastAPI(lifespan=lifespan)
+    app.include_router(user_router.router)
+    app.include_router(category_router.router)
+    app.include_router(product_router.router)
+    return app
 
-# Include the User Router
-app.include_router(user_router.router)
-app.include_router(category_router.router)
-app.include_router(product_router.router)
+app = create_app()
